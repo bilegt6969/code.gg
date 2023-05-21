137362071a1f62aefea8aa1300fbf28dce9016da
“Bilegt6969” <bilegt6969@gmail.com>
2023-05-22 02:57:23 +0800

    day four

--- app/html-attributes/Feed.tsx
+++ app/html-attributes/Feed.tsx
@@ -9,12 +9,12 @@ function Feed() {
           <li>Бүх HTML элементүүд нь атрибуттай байдаг.</li>
           <li>Атрибутууд нь элементийн талаар нэмэлт мэдээлэл өгдөг.</li>
           <li>Атрибутууд нь үргэлж эхлэх tag-т бичигддэг.</li>
-          <p>HTML-д холбоосыг &lt;<span className='text-red-400 bg-gray-700' dangerouslySetInnerHTML={{ __html: "a" }} />&gt; tag-д тодорхойлогддог. Холбоосыг <span className='text-pink-500'>href</span> атрибутад зааж өгнө.</p>
+          <p>HTML-д холбоосыг <span className='text-pink-500'>&lt;a&gt;</span> tag-д тодорхойлогддог. Холбоосыг <span className='text-pink-500'>href</span> атрибутад зааж өгнө.</p>
         </ul>
       </section>
       <section>
         <h1 className="font-bold mt-8 text-2xl">href</h1>
-        <p>HTML-д холбоосыг &lt;<span className='text-red-400 bg-gray-700' dangerouslySetInnerHTML={{ __html: "a" }} />&gt; tag-д тодорхойлогддог. Холбоосыг <span className='text-pink-500'>href</span> атрибутад зааж өгнө.</p>
+        <p>HTML-д холбоосыг <span className='text-pink-500'>&lt;a&gt;</span>  tag-д тодорхойлогддог. Холбоосыг <span className='text-pink-500'>href</span> атрибутад зааж өгнө.</p>
 
         <h1 className="font-bold mt-8 text-2xl">жишээ код:</h1>
         <div style={{ cursor: 'move' }} className="mt-4">
@@ -30,7 +30,7 @@ function Feed() {
       <section>
       <div className="mt-8">
       <h1 className="font-bold mt-8 text-2xl mb-4">src</h1>
-      <p>HTML-д зургийг &lt;<span className='text-red-400 bg-gray-700' dangerouslySetInnerHTML={{ __html: "img" }} />&gt; tag-д тодорхойлдог. Зургийн файлын байршлыг <span className='text-pink-500'>src</span> атрибутад зааж өгнө.</p>
+      <p>HTML-д зургийг <span className='text-pink-500'>&lt;img&gt;</span> tag-д тодорхойлдог. Зургийн файлын байршлыг <span className='text-pink-500'>src</span> атрибутад зааж өгнө.</p>
       <h1 className="font-bold mt-8 text-2xl mb-4">жишээ код:</h1>
 
             <iframe src="https://codesandbox.io/embed/musing-blackwell-5jhn7e?fontsize=14&hidenavigation=1&theme=dark&editorsize=50"
@@ -45,7 +45,7 @@ function Feed() {
       </section>
       <section>
       <h1 className="font-bold mt-8 mb-4 text-2xl">width, height</h1>
-      <p className='mb-4'>HTML-д зургийн өргөн болон өндрийг &lt;<span className='text-red-400 bg-gray-700' dangerouslySetInnerHTML={{ __html: "width" }} />&gt;, &lt;<span className='text-red-400 bg-gray-700' dangerouslySetInnerHTML={{ __html: "height" }} />&gt; атрибутад зааж өгдөг.</p>
+      <p className='mb-4'>HTML-д зургийн өргөн болон өндрийг <span className='text-pink-500'>&lt;width&gt;</span>,<span className='text-pink-500'>&lt;height&gt;</span> атрибутад зааж өгдөг.</p>
       <iframe src="https://codesandbox.io/embed/throbbing-moon-h95j89?fontsize=14&hidenavigation=1&theme=dark&editorsize=50"
   style={{ width: '100%', height: '500px', border: '0', borderRadius: '4px', overflow: 'hidden' }}
   title="throbbing-moon-h95j89"
@@ -61,7 +61,7 @@ function Feed() {
       </section>
       <section>
       <h1 className="font-bold mt-8 mb-4 text-2xl">alt</h1>
-      <p className='mb-4'>&lt;<span className='text-red-400 bg-gray-700' dangerouslySetInnerHTML={{ __html: "alt" }} />&gt; атрибутад зургийг харуулах боломжгүй үед оронд нь гарч ирэх тайлбар текcтыг зааж өгнө.</p>
+      <p className='mb-4'><span className='text-pink-500'>&lt;alt&gt;</span> атрибутад зургийг харуулах боломжгүй үед оронд нь гарч ирэх тайлбар текcтыг зааж өгнө.</p>
       <iframe src="https://codesandbox.io/embed/fervent-mopsa-5e5961?fontsize=14&hidenavigation=1&theme=dark&editorsize=50"
   style={{ width: '100%', height: '500px', border: '0', borderRadius: '4px', overflow: 'hidden' }}
   title="fervent-mopsa-5e5961"
@@ -72,7 +72,7 @@ function Feed() {
 
       <section>
       <h1 className="font-bold mt-8 mb-4 text-2xl">style</h1>
-      <p className='mb-4'>&lt;<span className='text-red-400 bg-gray-700' dangerouslySetInnerHTML={{ __html: "style" }} />&gt; атрибутад өнгө, үсгийн хэмжээ гэх мэт маш олон утгыг зааж өгч болдог. өнгө, үсгийн хэмжээ гэх мэт маш олон утгыг зааж өгч болдог.</p>
+      <p className='mb-4'><span className='text-pink-500'>&lt;style&gt;</span> атрибутад өнгө, үсгийн хэмжээ гэх мэт маш олон утгыг зааж өгч болдог. өнгө, үсгийн хэмжээ гэх мэт маш олон утгыг зааж өгч болдог.</p>
       <iframe src="https://codesandbox.io/embed/hardcore-lichterman-86vsxy?fontsize=14&hidenavigation=1&theme=dark&editorsize=50"
   style={{ width: '100%', height: '500px', border: '0', borderRadius: '4px', overflow: 'hidden' }}
   title="hardcore-lichterman-86vsxy"
@@ -83,7 +83,7 @@ function Feed() {
 
      <section>
       <h1 className="font-bold mt-8 mb-4 text-2xl">Lang</h1>
-      <p className='mb-4'>&lt;<span className='text-red-400 bg-gray-700' dangerouslySetInnerHTML={{ __html: "lang" }} />&gt; атрибутад &lt;<span className='text-red-400 bg-gray-700' dangerouslySetInnerHTML={{ __html: "html" }} />&gt;хуудасны хэлийг зааж өгнө. Хэлийг зарлах нь хайлтын системд чухал ач холбогдолтой байдаг</p>
+      <p className='mb-4'><span className='text-pink-500'>&lt;lang&gt;</span> атрибутад <span className='text-pink-500'>&lt;html&gt;</span> хуудасны хэлийг зааж өгнө. Хэлийг зарлах нь хайлтын системд чухал ач холбогдолтой байдаг</p>
       <iframe src="https://codesandbox.io/embed/rough-paper-0jku5d?fontsize=14&hidenavigation=1&theme=dark&editorsize=50"
   style={{ width: '100%', height: '500px', border: '0', borderRadius: '4px', overflow: 'hidden' }}
   title="rough-paper-0jku5d"
@@ -93,7 +93,7 @@ function Feed() {
      </section>
      <section>
       <h1 className="font-bold mt-8 mb-4 text-2xl">Title</h1>
-      <p className='mb-4'>&lt;<span className='text-red-400 bg-gray-700' dangerouslySetInnerHTML={{ __html: "p" }} />&gt; элементэд &lt;<span className='text-red-400 bg-gray-700' dangerouslySetInnerHTML={{ __html: "title" }} />&gt; атрибутыг ашиглаж үзье. Догол мөрд бичсэн текст дээр mouse очиход Title-ийн утга tooltip хэлбэрээр бидэнд харагдах болно</p>
+      <p className='mb-4'><span className='text-pink-500'>&lt;p&gt;</span> элементэд <span className='text-pink-500'>&lt;title&gt;</span> атрибутыг ашиглаж үзье. Догол мөрд бичсэн текст дээр mouse очиход Title-ийн утга tooltip хэлбэрээр бидэнд харагдах болно</p>
       <iframe src="https://codesandbox.io/embed/winter-grass-6i6n9o?fontsize=14&hidenavigation=1&theme=dark&editorsize=50"
   style={{ width: '100%', height: '500px', border: '0', borderRadius: '4px', overflow: 'hidden' }}
   title="winter-grass-6i6n9o"
